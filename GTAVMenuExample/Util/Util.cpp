10a15134ee15a73867ee7a1bfbf19b7e2337e69b
E66666666 <emily66666666ok@outlook.com>
2017-06-16 22:33:50 +0200

    Update to work with MenuBase refactor
    API changed/simplified!

--- GTAVMenuExample/Util/Util.cpp
+++ GTAVMenuExample/Util/Util.cpp
@@ -43,33 +43,11 @@ void showSubtitle(std::string message, int duration) {
 	UI::END_TEXT_COMMAND_PRINT(duration, 1);
 }
 
-GameSound::GameSound(char *sound, char *soundSet): m_prevNotification(0) {
-	Active = false;
-	m_sound = sound;
-	m_soundSet = soundSet;
-	m_soundID = -1;
+std::string prettyNameFromHash(Hash hash) {
+	char *name = VEHICLE::GET_DISPLAY_NAME_FROM_VEHICLE_MODEL(hash);
+	std::string displayName = UI::_GET_LABEL_TEXT(name);
+	if (displayName == "NULL") {
+		displayName = name;
+	}
+	return displayName;
 }
-
-GameSound::~GameSound() {
-	if (m_soundID == -1 || !Active) return;
-	AUDIO::RELEASE_SOUND_ID(m_soundID);
-}
-
-void GameSound::Load(char *audioBank) {
-	AUDIO::REQUEST_SCRIPT_AUDIO_BANK(audioBank, false);
-}
-
-void GameSound::Play(Entity ent) {
-	if (Active) return;
-	m_soundID = AUDIO::GET_SOUND_ID();
-	//showNotification(("New soundID: " + std::to_string(m_soundID)).c_str(), nullptr);
-	AUDIO::PLAY_SOUND_FROM_ENTITY(m_soundID, m_sound, ent, m_soundSet, 0, 0);
-	Active = true;
-}
-
-void GameSound::Stop() {
-	if (m_soundID == -1 || !Active) return;
-	AUDIO::STOP_SOUND(m_soundID);
-	Active = false;
-}
-
--- GTAVMenuExample/script.cpp
+++ GTAVMenuExample/script.cpp
@@ -1,42 +1,46 @@
+/*
+ * This file is an example implementation of the menu system.
+ */
+
 #include "script.h"
 
-#include <sstream>
+// You'll only need to include <menu.h> to start using it,
+// after setting paths up properly.
 #include <menu.h>
-#include <menucontrols.h>
 
+// You can ignore these includes as they're only used to resolve
+// paths, log things to a file or to provide nice wrappers for natives.
 #include "Util/Paths.h"
 #include "Util/Util.hpp"
 #include "Util/Logger.hpp"
+#include "Util/Versions.h"
 
-#include "settings.h"
-
-std::string prettyNameFromHash(Hash hash) {
-	char *name = VEHICLE::GET_DISPLAY_NAME_FROM_VEHICLE_MODEL(hash);
-	std::string displayName = UI::_GET_LABEL_TEXT(name);
-	if (displayName == "NULL") {
-		displayName = name;
-	}
-	return displayName;
-}
+NativeMenu::Menu menu;
+std::string settingsMenuFile;
 
 Player player;
 Ped playerPed;
-Vehicle vehicle;
-Hash model;
 
-Settings settings;
-
-NativeMenu::Menu menu;
-NativeMenu::MenuControls controls;
-std::string settingsMenuFile;
-
+// A few state variables
 bool checkBoxStatus = false;
 float someFloat = 0.0f;
-float floatStep = 0.01f;
 int someInt = 0;
 int intStep = 1;
-
 int stringsPos = 0;
+int stepChoice = 0;
+
+// Choice of step size to demonstrate variable precision display
+std::vector<float> floatSteps = {	
+	1.0f,
+	0.1f, 
+	0.01f, 
+	0.001f, 
+	0.0001f, 
+	0.00001f,
+	0.000001f,
+};
+
+// Random words to go through in the menu
 std::vector<std::string> strings = {
 	"Here",
 	"are",
@@ -48,10 +52,11 @@ std::vector<std::string> strings = {
 	"from",
 };
 
+/*
+ * The settings may be read multiple times if needed.
+ */
 void init() {
-	// TODO: Separately reading in files is a bit dirty, this'll need some work.
-	settings.ReadSettings(&controls, &menu);
-	menu.LoadMenuTheme(std::wstring(settingsMenuFile.begin(), settingsMenuFile.end()).c_str());
+	menu.ReadSettings();
 	logger.Write("Settings read");
 	logger.Write("Initialization finished");
 }
@@ -64,13 +69,15 @@ void onMain() {
 }
 
 /*
- * This simple function is executed when the menu closes. You can handle things
- * you temporarily stored in the menu, for example.
+ * This simple function is executed when the menu closes.
  */
 void onExit() {
 	logger.Write("Menu was closed");
 }
 
+/*
+ * We'll attach these functions in an OptionPlus method.
+ */
 void onRight() {
 	showNotification("You pressed RIGHT on an OptionPlus");
 }
@@ -82,20 +89,35 @@ void onLeft() {
 /*
  * update_menu() should be called each tick.
  */
-
 void update_menu() {
-	// Each tick, the controls are checked. If the key is hit to open
-	// or close the menu, the binded functions are called.
-	menu.CheckKeys(&controls, std::bind(onMain), std::bind(onExit));
+	/*
+	 * Each tick, the controls are checked. If the key is hit to open
+	 * or close the menu, the binded functions are called.
+	 * This function has to be called for navigation to work.
+	 */
+	menu.CheckKeys();
 
-	// You can define a menu like this. The main menu should always be
-	// called "mainmenu".
+	/*
+	 *  You can define a menu like this. 
+	 *  The main menu should always be called "mainmenu".
+	 */ 
 	if (menu.CurrentMenu("mainmenu")) {
 		// The title is NOT optional.
-		menu.Title("Example!");
+		menu.Title("Menu example");
+
+		// This is a normal option. It'll return true when "select" is presed.
+		if (menu.Option("Click me!", { "This will log something to " + Paths::GetModuleNameWithoutExtension() + ".log" })) {
+			showNotification("Check the logfile!");
+			logger.Write("\"Click me!\" was selected!");
+		}
 
 		// This will open a submenu with the name "submenu"
 		menu.MenuOption("Look, a submenu!", "submenu", { "This submenu demonstrates a few settings."});
+
+		// Showing static information is also possible if a string vector only contains one element.
+		int nothing = 0;
+		menu.StringArray("Version", { DISPLAY_VERSION }, nothing, 
+						 { "Thanks for checking out this menu!", "-ikt",  eGameVersionToString(getGameVersion())});
 	}
 
 	// Any submenus can have any titles. They should only need to match
@@ -103,17 +125,12 @@ void update_menu() {
 	if (menu.CurrentMenu("submenu")) {
 		menu.Title("I'm a submenu!");
 
-		if (menu.Option("Click me!", { "This will log something to " + Paths::GetModuleNameWithoutExtension() + ".log" })) {
-			showNotification("Check the logfile!");
-			logger.Write("\"Click me!\" was selected!");
-		}
-
-		menu.BoolOption("Here's a checkbox", &checkBoxStatus, { std::string("Boolean is ") + (checkBoxStatus ? "checked" : "not checked") + "." });
-		menu.IntOption("Ints!", &someInt, -100, 100, intStep, { "Stepsize can be changed!" });
-		menu.IntOption("Int step size", &intStep, 1, 100, 1, { "Stepsize can be changed!" });
-		menu.FloatOption("Floats?", &someFloat, -100.0f, 100.0f, floatStep, { "Try holding left/right, things should speed up." });
-		menu.FloatOption("Float step size", &floatStep, 0.01f, 100.0f, 0.01f, { "Steps smaller than 0.01 aren't visible until they go over the 0.01 mark." });
-		menu.StringArray("String arrays", strings, &stringsPos, { "You can also show different strings" });
+		menu.BoolOption("Here's a checkbox", checkBoxStatus, { std::string("Boolean is ") + (checkBoxStatus ? "checked" : "not checked") + "." });
+		menu.IntOption("Ints!", someInt, -100, 100, intStep, { "Stepsize can be changed!" });
+		menu.IntOption("Int step size", intStep, 1, 100, 1, { "Stepsize can be changed!" });
+		menu.FloatOption("Floats?", someFloat, -100.0f, 100.0f, floatSteps[stepChoice], { "Try holding left/right, things should speed up." });
+		menu.FloatArray("Float step size", floatSteps, stepChoice, { "Something something magic!" });
+		menu.StringArray("String arrays", strings, stringsPos, { "You can also show different strings" });
 
 		menu.Option("Description info",
 		{ "You can put arbitarily long texts in the description. "
@@ -130,7 +147,7 @@ void update_menu() {
 			"Each string is a new line",
 			"The box expands by itself"
 		};
-		menu.OptionPlus("Look to the right!", extraInfo, nullptr, std::bind(onLeft), std::bind(onRight), "Something", 
+		menu.OptionPlus("Look to the right!", extraInfo, std::bind(onLeft), std::bind(onRight), "Something", 
 		{"You do need to manage the line splitting yourself, as it's meant for short pieces of info."});
 	}
 
@@ -154,17 +171,19 @@ void update_game() {
 		return;
 	}
 
-	vehicle = PED::GET_VEHICLE_PED_IS_IN(playerPed, false);
-	model = ENTITY::GET_ENTITY_MODEL(vehicle);
+	/* Whatever happens normally in your script! */
 }
 
 void main() {
 	logger.Write("Script started");
 
 	settingsMenuFile = Paths::GetModuleFolder(Paths::GetOurModuleHandle()) + modDir + "\\settings_menu.ini";
-	settings.SetFiles("", settingsMenuFile);
+	menu.SetFiles(settingsMenuFile);
 
 	logger.Write("Loading " + settingsMenuFile);
+
+	menu.RegisterOnMain(std::bind(onMain));
+	menu.RegisterOnExit(std::bind(onExit));
 
 	init();
 
